016d247ac944a3715097017b470d82e9feab14be
preeti-puja <preeti.puja18@gmail.com>
2018-02-21 17:15:07 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// create a new component this component should produce some html
+
+// take this component generated html and put it 
+// on the page
+
+const App = function(){
+    return <div>Hello!</div>
+}
+
+//take this component and show on page
+React.render(App)
